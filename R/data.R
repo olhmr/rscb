e86f1b277e92a1d92978e74584e17249a2488072
Olle Hammarstrom <olle.hammarstrom@gmail.com>
2019-03-20 23:20:52 +0000

    Renamed scb_create_cache to scb_create_directory_cache() Added scb_table_cache() and saved data (failed)

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Data frame containing information on all directories, subdirectories, and
 #' tables in the ssd database (the main database for national statistics, and
 #' currently the only one accessible via API).
-#' 
+#'
 #' @format A data frame with ca 2797 rows and 4 columns:
 #' \describe{
 #'   \item{id}{id to use to query directory or table}
@@ -11,5 +11,24 @@
 #'   \item{depth}{1 = highest level directory, incremented for each subdirectory}
 #'   \item{text}{Information on database / table}
 #' }
-#' @source rscb::scb_create_cache()
+#' @source rscb::scb_create_directory_cache()
 "scb_directory_cache"
+#' Cached view of ssd tables
+#'
+#' Data frame containing metadata for all tables in ssd database (the main
+#' database for national statistics, and currently the only one accessible via
+#' API).
+#'
+#' @format A data frame with x rows and 8 columns:
+#' \describe{
+#'   \item{id}{id to use to query table}
+#'   \item{name}{name of table}
+#'   \item{code}{variable code}
+#'   \item{text}{description of variable}
+#'   \item{values}{possible values for variable}
+#'   \item{valueText}{description of value}
+#'   \item{elimination}{aggregation behaviour}
+#'   \item{time}{indicates variable contains time data}
+#' }
+#' @source rscb::scb_create_table_cache()
+"scb_table_cache"
