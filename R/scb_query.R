a7d5ddcc6704dbfb6a62cebe8f81aab327b9f7ed
Olle Hammarstrom <olle.hammarstrom@gmail.com>
2019-03-28 13:21:15 +0000

    Mans ok

--- R/scb_query.R
+++ R/scb_query.R
@@ -1,10 +1,12 @@
 #' Query a table in the SCB Database
 #'
-#' This is essentially a wrapper for httr::POST, with some more intuitive and
-#' user-friendly handling of arguments and output. The ... arguments should be
-#' named lists of the form list(code = variable_code_to_filter_on, filter =
-#' type_of_filter_to_use, values = values_to_filter_on). These are converted to
-#' JSON with the jsonlite package, and submitted as the body in httr::POST.
+#' This is essentially a wrapper for \code{\link[httr]{POST}}, with some more
+#' intuitive and user-friendly handling of arguments and output. The ...
+#' arguments should be named lists of the form list(code =
+#' variable_code_to_filter_on, filter = type_of_filter_to_use, values =
+#' values_to_filter_on). These are converted to JSON with the
+#' \href{https://cran.r-project.org/web/packages/jsonlite/index.html}{jsonlite}
+#' package, and submitted as the body in \code{\link[httr]{POST}}.
 #'
 #' The types of filter that can be used are: \itemize{ \item item - filter to
 #' values matching those provided \item all - wildcard selection, e.g. \* for
@@ -12,12 +14,14 @@
 #' top x records, where x is provided in values \item agg - aggregate values
 #' \item vs - comparison versus other variable }
 #'
+#' For more information, see \url{https://www.scb.se/api/}.
+#'
 #' @param table_id ID path of table to query
 #' @param lang Language "en" English or "sv" Swedish
 #' @param database_id ID of database; currently only "ssd"
 #' @param ... Arguments to query table with, each like: list(code =
 #'   code_to_query, filter = filter_type, values = c(values_to_filter))
-#' @return data.frame containing response from POST query
+#' @return data.frame containing response from \code{\link[httr]{POST}} query
 #' @export
 scb_query <- function(table_id, ..., lang = "en", database_id = "ssd") {
 
@@ -33,19 +37,19 @@ scb_query <- function(table_id, ..., lang = "en", database_id = "ssd") {
 
   # Iterate through argument list
   if (length(args) > 0) {
-    
+
     for (i in 1:length(args)) {
-      
+
       # Main data.frame
       main_frame <- rbind(main_frame, data.frame(code = args[[i]]$code, stringsAsFactors = FALSE))
-      
+
       # Selection data.frame
       tmp_sel <- data.frame(filter = args[[i]]$filter, stringsAsFactors = FALSE)
       tmp_sel$values <- list(args[[i]]$values)
       sel_frame <- rbind(sel_frame, tmp_sel)
-      
+
     }
-    
+
   }
 
   # Join up
@@ -68,13 +72,13 @@ scb_query <- function(table_id, ..., lang = "en", database_id = "ssd") {
 
   # Check and format response
   if (response$status_code == 200) {
-    
+
     output <- httr::content(response)
-    
+
   } else {
-    
+
     output <- paste0("Unexpected status code from POST: ", response$status_code)
-    
+
   }
 
   # Return results
--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,4 @@
-# Utility functions - do not export
-
-#' Keep track of API calls
+#' Manage API calls in last 10 seconds
 #'
 #' Manages list of all API calls made in the last 10 seconds. If no tracker is
 #' provided, a new one is returned. If an already existing tracker is passed as
@@ -8,198 +6,167 @@
 #' remove all items with a timestamp > 10 seconds from current system time.
 #'
 #' The SCB API limits calls to 10 per 10 seconds (rolling). If this number is
-#' exceeded, httr::GET returns a 429 response. We could use Sys.sleep(10) when
-#' this occurs to reset the counter, but this would be inefficient: since the
-#' limit is rolling, we should only wait until the oldest call is > 10 seconds
-#' in the past, and then attempt a new query. This is what this function
-#' facilitates.
+#' exceeded, \code{\link[httr]{GET}} returns a 429 response. We could use
+#' \code{\link[base]{Sys.sleep}} with time set to 10 seconds when this occurs,
+#' to reset the counter, but this would be inefficient: since the limit is
+#' rolling, we should only wait until the oldest call is > 10 seconds in the
+#' past, and then attempt a new query. This is what this function facilitates.
 #'
 #' @param tracker Currently tracked calls
-#' @return Data frame containing currently tracked calls
+#' @return data.frame containing currently tracked calls
 update_call_tracker <- function(tracker = NULL) {
-  
+
   if (is.null(tracker)) {
-    
+
     # No tracker set up, create new
     tracker <- data.frame(timestamp = Sys.time(),
                           stringsAsFactors = FALSE)
-    
+
   } else {
-    
+
     # Add to current tracker
     tracker <- rbind(tracker, data.frame(timestamp = Sys.time(),
                                          stringsAsFactors = FALSE))
-    
+
     # Delete records older than 10 seconds
     tracker <- tracker[difftime(Sys.time(), tracker$timestamp) < 10, , drop = FALSE]
-    
+
   }
-  
+
   # Return new tracker
   return(tracker)
-  
+
 }
-#' Convert metadata to intelligible data frame
+#' Extract pertinent information from table variables metadata
 #'
-#' Superceded by interpret_table_variables(), which provides more efficient
-#' storage of the pertinent data contained in the metadata. Kept here in case a
-#' new use for it is discovered, but will likely be removed in a future update.
+#' Calling \code{\link{scb_list}} with a table ID as argument will return a
+#' 2-element list containing metadata; the title of the table, and a list of
+#' variables and field values present in the table. This function takes the
+#' variable portion of the metadata and extracts the most useful information
+#' from it, which is then stored in a data.frame for easy access.
 #'
-#' @param id ID of table
-#' @param metadata Metadata returned from scb_list() call to table ID
-#' @return Data table containing id, name, variables, and values for table
-interpet_table_metadata <- function(id, metadata) {
-  
-  # Store table title
-  table_name <- metadata$title
-  
-  # Convert variables to data.table to make unlisting easy
-  table_vars <- data.frame(metadata$variables, stringsAsFactors = FALSE)
-  
-  # Check existence of fields - add dummy if not
-  if (!"code" %in% names(table_vars)) {table_vars$code <- NA}
-  if (!"text" %in% names(table_vars)) {table_vars$text <- NA}
-  if (!"values" %in% names(table_vars)) {table_vars$values <- NA}
-  if (!"valueTexts" %in% names(table_vars)) {table_vars$valueTexts <- NA}
-  if (!"elimination" %in% names(table_vars)) {table_vars$elimination <- NA}
-  if (!"time" %in% names(table_vars)) {table_vars$time <- NA}
-  
-  # Unlist fields
-  table_vars <- tidyr::unnest(table_vars)
-  
-  # Bind together
-  table_joined <- cbind(id, table_name, table_vars)
-  
-  # Return
-  return(table_joined)
-  
-}
-#' Extract pertinent information from table metadata
-#'
-#' Calling scb_list() with a table ID as argument will return a 2-element list
-#' containing metadata; the title of the table, and a list of variables and
-#' field values present in the table. This function takes the variable portion
-#' of the metadata and extracts the most useful information from it, which is
-#' then stored in a data.frame for easy access.
-#'
-#' @param table_vars From scb_list() call to table ID (select $variables)
+#' @param table_vars From \code{\link{scb_list}} call to table ID (select
+#'   $variables)
 #' @return data.frame containing variable and value descriptions, as well as
 #'   start and end of date range for data
 interpret_table_variables <- function(table_vars) {
-  
+
   # Create output container
   extract <- data.frame(variable_descriptions = NA, value_descriptions = NA,
                         date_range_start = NA, date_range_end = NA, stringsAsFactors = FALSE)
   time_index <- NA
-  
+
   # Find time component; if none, keep NA
   # Relies on fact that only one variable can be time
   # Replace with listing of years with data (no months)
   if ("time" %in% names(table_vars)) {
-    
+
     # Store time index
     time_index <- which(table_vars$time)
-    
+
     # Store time values
     time_values <- table_vars$values[time_index]
-    
+
     # Assume ordered list for now
     time_start <- time_values[[1]][1]
     time_end <- time_values[[1]][length(time_values[[1]])]
-    
+
   }
-  
+
   # Find text description of variables, excluding time
   if ("text" %in% names(table_vars)) {
-    
+
     if (is.na(time_index)) {
-      
+
       variable_descriptions <- list(table_vars$text)
-      
+
     } else {
-      
+
       variable_descriptions <- list(table_vars$text[-time_index])
-      
+
     }
-    
+
   }
-  
+
   # Find text description of variable values, excluding time
   if ("valueTexts" %in% names(table_vars)) {
-    
+
     if (is.na(time_index)) {
-      
+
       value_descriptions <- list(table_vars$valueTexts)
-      
+
     } else {
-      
+
       value_descriptions <- list(table_vars$valueTexts[-time_index])
-      
+
     }
-    
+
   }
-  
+
   extract$variable_descriptions <- variable_descriptions
   extract$value_descriptions <- value_descriptions
   extract$date_range_start <- time_start
   extract$date_range_end <- time_end
-  
+
   return(extract)
-  
+
 }
-#' Try scb_list() and catch 429 response
+#' Try \code{\link{scb_list}} and catch 429 response
 #'
-#' This is a wrapper for scb_list() to enable it to call repeatedly in case of a
-#' 429 (too many requests) response is returned. Since the SCB API has a rolling
-#' limit of 10 call per 10 seconds, this situation will frequently occur. In
-#' case the response is neither a data.frame or list, the function will throw an
-#' error.
+#' This is a wrapper for \code{\link{scb_list}} to enable it to call repeatedly
+#' in case of a 429 (too many requests) response is returned. Since the SCB API
+#' has a rolling limit of 10 call per 10 seconds, this situation will frequently
+#' occur. In case the response is neither a data.frame or list, the function
+#' will throw an error.
 #'
-#' There is potential to improve this, in combination with scb_list(), by having
-#' the latter return the status code along with the data. 
+#' There is potential to improve this, in combination with
+#' \code{\link{scb_list}}, by having the latter return the status code along
+#' with the data. In addition, this will likely be merged into
+#' \code{\link{scb_list}} in the future.
 #'
-#' @param lang Language
-#' @param database_id Database
-#' @param id ID to query
-#' @param call_tracker Current call tracker
-#' @return Valid response from scb_list()
+#' @param lang "en" for English, "sv" for Swedish
+#' @param database_id Database to search
+#' @param id Path to search in database
+#' @param call_tracker Current call tracker: see
+#'   \code{\link[rscb]{update_call_tracker}}
+#' @return Result of \code{\link{scb_list}} call
 try_scb_list <- function(lang, database_id, id, call_tracker) {
-  
+
   # Call scb_list: if 429 response, wait for cache to clear then continue
   counter <- 0 # If this reaches 50000, exit: prevents infinite loops
   while (TRUE) {
-    
+
     call_tracker <- update_call_tracker(call_tracker)
     cur_dir <- scb_list(lang = lang,
                         database_id = database_id,
                         id = id)
-    
+
     if (!is.data.frame(cur_dir) & !is.list(cur_dir)) {
-      
+
       counter <- counter + 1
       if (cur_dir == "Unexpected status code from GET: 429") {
-        
+
         # Wait for call_tracker to clear
         time_to_sleep <- difftime(Sys.time(), call_tracker[which.min(call_tracker$timestamp), ])
         Sys.sleep(time_to_sleep)
-        
+
       } else {
-        
+
         stop("Unknown error in scb_list() call in add_directory_to_cache()")
-        
+
       }
-      
+
     } else {
-      
+
       counter <- 0
       break
-      
+
     }
-    
+
     if (counter > 49999) {stop("Loop failed to exit in try_scb_list()")}
-    
+
   }
-  
+
   return(cur_dir)
-  
+
 }
