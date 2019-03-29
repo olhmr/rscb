8bff2399bc734d059e518607daa3349f93cec432
Olle Hammarstrom <olle.hammarstrom@gmail.com>
2019-03-29 19:31:40 +0000

    Expanded tests, including comparing cache structure to old

--- tests/testthat/test_scb_create_cache.R
+++ tests/testthat/test_scb_create_cache.R
@@ -1,6 +1,6 @@
-context("Caching: scb_create_cache")
+context("Caching: scb_create_cache and cache integrity")
 
-testthat::test_that("a small subset of the database can be cached and searched", {
+testthat::test_that("a small subset of the database can be cached and accessed", {
 
   testthat::expect_true(is.data.frame(scb_create_cache(initial_id = "AM/AM0101")))
   scb_cache_test <- scb_create_cache(initial_id = "AM/AM0101")
@@ -13,5 +13,42 @@ testthat::test_that("unexpected arguments give intelligible errors", {
 
   testthat::expect_warning(scb_create_cache(lang = "kr"))
   testthat::expect_warning(scb_create_cache(initial_id = 3))
+
+})
+
+testthat::test_that("the cache has all expected columns", {
+
+  scb_cache_test <- scb_create_cache(initial_id = "AM/AM0101")
+  testthat::expect_true("id" %in% names(scb_cache_test))
+  testthat::expect_true("depth" %in% names(scb_cache_test))
+  testthat::expect_true("type" %in% names(scb_cache_test))
+  testthat::expect_true("name" %in% names(scb_cache_test))
+  testthat::expect_true("var_desc" %in% names(scb_cache_test))
+  testthat::expect_true("val_desc" %in% names(scb_cache_test))
+  testthat::expect_true("date_start" %in% names(scb_cache_test))
+  testthat::expect_true("date_end" %in% names(scb_cache_test))
+
+})
+
+testthat::test_that("tables, and only tables, have metadata", {
+
+  scb_cache_test <- scb_create_cache(initial_id = "AM/AM0101")
+  testthat::expect_equal(nrow(scb_cache_test[scb_cache_test$type == "l" &
+                                             (!is.na(scb_cache_test$var_desc)) &
+                                             (!is.na(scb_cache_test$val_desc)) &
+                                             (!is.na(scb_cache_test$date_start)) &
+                                             (!is.na(scb_cache_test$date_end)), ]), 0)
+  testthat::expect_equal(nrow(scb_cache_test[scb_cache_test$type == "t" &
+                                               (is.na(scb_cache_test$var_desc)) &
+                                               (is.na(scb_cache_test$val_desc)) &
+                                               (is.na(scb_cache_test$date_start)) &
+                                               (is.na(scb_cache_test$date_end)), ]), 0)
+
+})
+
+testthat::test_that("the same cache is created from the same input", {
+
+  testthat::expect_equal_to_reference(scb_create_cache(initial_id = "AM/AM0101"),
+                                      file = load("scb_cache_test.rda"))
 
 })
