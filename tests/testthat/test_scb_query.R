1b0909f05ae475f2971e143e3ca9ae2d9455be16
Olle Hammarstrom <olle.hammarstrom@gmail.com>
2019-03-29 19:31:58 +0000

    Basic scb_query tests

---
+++ tests/testthat/test_scb_query.R
@@ -0,0 +1,28 @@
+context("Quering: scb_query")
+
+testthat::test_that("unexpected arguments give warnings", {
+
+  testthat::expect_warning(scb_query(table_id = "AM/AM0101/AM0101A/LonArb07Privat",
+                                     list(code = "Overtidstillagg", filter = "top", values = "2"),
+                                     lang = "kr"))
+  testthat::expect_warning(scb_query(table_id = "AM/AM0101/AM0101A/LonArb07Privat",
+                                     list(code = "Overtidstillagg", filter = "top", values = "2"),
+                                     database_id = "ssb"))
+  testthat::expect_warning(scb_query(table_id = "AM/AM0101/AM0101A/LonArb07Privat"))
+
+})
+
+testthat::test_that("using two filters give the intersection of each filter", {
+
+  # This relies on the elimination procedure explained in the API documentation
+  # at www.scb.se/api.
+
+  filter_1 <- scb_query(table_id = "AM/AM0101/AM0101A/LonArb07Privat",
+                        list(code = "SNI2007", filter = "item", values = c("B", "C", "D")))
+  intersection <- as.data.frame(filter_1[filter_1$`overtime pay` == "including overtime pay", ])
+  combined <- as.data.frame(scb_query(table_id = "AM/AM0101/AM0101A/LonArb07Privat",
+                                      list(code = "Overtidstillagg", filter = "item", values = "20"),
+                                      list(code = "SNI2007", filter = "item", values = c("B", "C", "D"))))
+  testthat::expect_equivalent(intersection, combined)
+
+})
